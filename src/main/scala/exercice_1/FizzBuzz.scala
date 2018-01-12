21bfea4dfb1fdc452b420f664d4288482d317297
c.verdier <c.verdier.ext@meetic-corp.com>
2018-01-12 10:00:52 +0100

    Solution exercice 1

--- src/main/scala/exercice_1/FizzBuzz.scala
+++ src/main/scala/exercice_1/FizzBuzz.scala
@@ -10,7 +10,12 @@ object FizzBuzz extends App {
     *  - the same number (as String) if no other requirement is fulfilled
     */
   def getResult(number: Int): String = {
-    ???
+    (number % 3, number % 5) match {
+      case (0, 0) => "fizzbuzz"
+      case (0, _) => "fizz"
+      case (_, 0) => "buzz"
+      case (_, _) => number.toString
+    }
   }
 
   private val sample = List(1, 3, 4, 6, 15, 10, 2, 15, 22, 30, 25, 17)

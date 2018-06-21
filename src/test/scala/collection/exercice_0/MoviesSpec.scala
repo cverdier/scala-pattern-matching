d1e6597be254bada29481a8c8ec18b8df2ba9c7e
Jean-Marie Lamodière <jmlamo@gmail.com>
2018-06-21 17:39:17 +0200

    test: filter movies by multiple filters

--- src/test/scala/collection/exercice_0/MoviesSpec.scala
+++ src/test/scala/collection/exercice_0/MoviesSpec.scala
@@ -25,5 +25,27 @@ class MoviesSpec extends WordSpec with Matchers {
         MovieRelease("Ran", 1985)
       ))
     }
+
+    "filter movies released before 1960" in {
+      val result = Movies.getMoviesWithMultipleFilters(Movies.someMovies)(List(
+        _.releaseYear < 1960
+      ))
+
+      result.map(movie => (movie.name, movie.releaseYear)) should be(List(
+        ("Rashomon", 1950),
+        ("Vertigo", 1958)
+      ))
+    }
+
+    "filter movies released before 1960 by Hitchcock" in {
+      val result = Movies.getMoviesWithMultipleFilters(Movies.someMovies)(List(
+        _.releaseYear < 1960,
+        _.director == Director.Hitchcock
+      ))
+
+      result.map(movie => (movie.name, movie.releaseYear)) should be(List(
+        ("Vertigo", 1958)
+      ))
+    }
   }
 }

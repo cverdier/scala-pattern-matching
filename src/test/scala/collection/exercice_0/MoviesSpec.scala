b050c823218679cfd89244b1d64fb47e69e45317
Jean-Marie Lamodière <jmlamo@gmail.com>
2018-06-21 17:24:12 +0200

    test: filter movies by director

--- src/test/scala/collection/exercice_0/MoviesSpec.scala
+++ src/test/scala/collection/exercice_0/MoviesSpec.scala
@@ -1,7 +1,17 @@
 package collection.exercice_0
 
-import org.scalatest.WordSpec
+import collection.exercice_0.model.Director
+import org.scalatest.{Matchers, WordSpec}
 
-class MoviesSpec extends WordSpec {
+class MoviesSpec extends WordSpec with Matchers {
+  "a Movies" should{
+    "filter movies by director" in {
+      val result = Movies.getMoviesMadeBy(Movies.someMovies, Director.Hitchcock)
 
+      result.map(_.name) should be(List(
+        "Psycho",
+        "Vertigo"
+      ))
+    }
+  }
 }

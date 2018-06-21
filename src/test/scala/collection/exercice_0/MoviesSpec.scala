c1ecd1f44ac761f9ead91787d42b8044adb42813
Jean-Marie Lamodière <jmlamo@gmail.com>
2018-06-21 17:30:38 +0200

    test: retrieve releases sorted by year

--- src/test/scala/collection/exercice_0/MoviesSpec.scala
+++ src/test/scala/collection/exercice_0/MoviesSpec.scala
@@ -1,5 +1,6 @@
 package collection.exercice_0
 
+import collection.exercice_0.Movies.MovieRelease
 import collection.exercice_0.model.Director
 import org.scalatest.{Matchers, WordSpec}
 
@@ -11,6 +12,17 @@ class MoviesSpec extends WordSpec with Matchers {
       result.map(_.name) should be(List(
         "Psycho",
         "Vertigo"
+      ))
+    }
+
+    "retrieve releases sorted by year" in {
+      val result = Movies.getSortedMovieReleases(Movies.someMovies)
+
+      result should be(List(
+        MovieRelease("Rashomon", 1950),
+        MovieRelease("Vertigo", 1958),
+        MovieRelease("Psycho", 1960),
+        MovieRelease("Ran", 1985)
       ))
     }
   }
